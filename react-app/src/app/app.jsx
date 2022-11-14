70b28bfa95a2a56484b341a7b8eecf5b2211f39a
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-11-15 00:40:58 +0300

    Add minor structure changes

---
+++ react-app/src/app/app.jsx
@@ -0,0 +1,18 @@
+import useImportScript from '../hooks/useImportScript';
+import PageProduct from '../components/product/product';
+import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
+
+function App() {
+    useImportScript('/js/page-product.js');
+
+    return (
+        <div className="app font">
+            <Header />
+            <PageProduct />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import './app/app.scss';
+import App from './app/app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
