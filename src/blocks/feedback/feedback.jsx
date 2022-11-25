a70906a862fa035360a2cc93c7cf7276d163e533
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-11-25 12:59:51 +0300

    Start transferring form validation to react

--- src/blocks/feedback/feedback.jsx
+++ src/blocks/feedback/feedback.jsx
@@ -1,6 +1,36 @@
 import Btn from "../btn/btn";
 
 function Feedback() {
+    let objectForm = {
+        fullname: {
+            id: 'fullname',
+            value: undefined,
+            error: false,
+            errorList: { 
+                empty: 'Вы забыли указать имя и фамилию',
+                tooShort: 'Имя не может быть короче 2-х символов',
+            },
+        },
+        rating: {
+            id: 'rating',
+            value: undefined,
+            error: false,
+            errorList: { 
+                notInRange: 'Оценка должна быть от 1 до 5',
+            },
+        },
+        comment: { 
+            id: 'comment',
+            value: undefined,
+            error: false,
+            errorList: {},
+        },
+    };
+
+    const handleClickSubmit = () => {
+        
+    }
+
     return (
         <div className="feedback">
             <div className="feedback__aside"></div>
@@ -35,7 +65,7 @@ function Feedback() {
                         <textarea className="feedback__textarea form__textarea form__field" name="feedback" placeholder="Текст отзыва" id="comment"></textarea>
                         <div className="form__field-error hidden"></div>
                     </div>
-                    <Btn className="form__submit">
+                    <Btn className="form__submit" onClick={handleClickSubmit}>
                         Отправить отзыв
                     </Btn>
                 </form>
--- src/pages/page-product/page-product.jsx
+++ src/pages/page-product/page-product.jsx
@@ -1,12 +1,12 @@
 import { breadcrumbsItems } from '../../data/data';
-import useImportScript from '../../hooks/useImportScript';
+// import useImportScript from '../../hooks/useImportScript';
 import Breadcrumbs from '../../blocks/breadcrumbs/breadcrumbs';
 import Product from '../../blocks/product/product';
 import Header from '../../blocks/header/header';
 import Footer from '../../blocks/footer/footer';
 
 function PageProduct() {
-    useImportScript('/js/page-product.js');
+    // useImportScript('/js/page-product.js');
 
     return (
         <>
