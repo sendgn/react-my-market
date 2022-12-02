8e1c9b0fbfe16291c711e7407028b88ceb31398a
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-12-03 01:45:11 +0300

    Add favorites reducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './reducers/cart-reducer'; 
+import favoritesReducer from './reducers/favorites-reducer';
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
+        favorites: favoritesReducer
     },
 });
