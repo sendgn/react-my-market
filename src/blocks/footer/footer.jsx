a6223fe549e67ebce2fc9a9543a6c156f7365b1a
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2022-12-03 03:07:13 +0300

    Add Link component

--- src/blocks/footer/footer.jsx
+++ src/blocks/footer/footer.jsx
@@ -1,4 +1,5 @@
-import { useCurrentDate } from "@kundinos/react-hooks";
+import { useCurrentDate } from '@kundinos/react-hooks';
+import Link from '../link/link';
 
 function Footer() {
     const currentDate = useCurrentDate();
@@ -9,10 +10,10 @@ function Footer() {
             <div className="footer__content container">
                 <div className="footer__info">
                     <p><b>&copy; ООО «<span className="colored">Мой</span>Маркет», 2018-{`${fullYear}`}.</b></p>
-                    <p>Для уточнения информации звоните по номеру <a className="link" href="tel:79000000000">+7 900 000 0000</a>,</p>
-                    <p>а предложения по сотрудничеству отправляйте на почту <a className="link" href="mailto:partner@mymarket.com">partner@mymarket.com</a></p>
+                    <p>Для уточнения информации звоните по номеру <Link href="tel:79000000000">+7 900 000 0000</Link>,</p>
+                    <p>а предложения по сотрудничеству отправляйте на почту <Link href="mailto:partner@mymarket.com">partner@mymarket.com</Link></p>
                 </div>
-                <a className="link" href="#top">Наверх</a>
+                <Link href="#top">Наверх</Link>
             </div>
         </footer>
     );
